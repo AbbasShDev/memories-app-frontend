de3dbd27f37131e8a70612891dc561a3009f1f0e
abbasShDev <abbas20alzaeem@gmail.com>
2021-09-16 17:20:15 +0300

    Auth form done

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "./Input";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const classess = useStyles();
+
+  const handelSubmit = () => {};
+
+  const handelChange = () => {};
+
+  const handelShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handelShowPassword(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classess.paper} elevation={3}>
+        <Avatar className={classess.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classess.form} onSubmit={handelSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handelChange={handelChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="familyName"
+                  label="Family Name"
+                  handelChange={handelChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email"
+              handelChange={handelChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handelChange={handelChange}
+              type={showPassword ? "text" : "password"}
+              handelShowPassword={handelShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handelChange={handelChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classess.submit}
+          >
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? "Already have an account? Sign In"
+                  : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
---
+++ src/components/auth/Input.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { TextField, Grid, InputAdornment, IconButton } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
+const Input = ({
+  name,
+  label,
+  autoFocus,
+  type,
+  half,
+  handelChange,
+  handelShowPassword,
+}) => {
+  return (
+    <Grid item sx={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        onChange={handelChange}
+        variant="outlined"
+        required
+        fullWidth
+        label={label}
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password" && {
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handelShowPassword}>
+                  {type === "password" ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }
+        }
+      ></TextField>
+    </Grid>
+  );
+};
+
+export default Input;
