197acc2c13ef31c633f6c29858a2bcc3e8e3eabd
abbasShDev <abbas20alzaeem@gmail.com>
2021-09-29 16:09:42 +0300

    Modify routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
 import Auth from "./components/auth/Auth";
+import PostDetails from "./components/postDetails/PostDetails";
 
 const App = () => {
+  const { authData: user } = useSelector((state) => state.auth);
+
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/auth" exact component={Auth} />
+          <Route path="/" exact component={() => <Redirect to="posts" />} />
+          <Route path="/posts" exact component={Home} />
+          <Route path="/posts/search" exact component={Home} />
+          <Route path="/posts/:id" exact component={PostDetails} />
+          <Route
+            path="/auth"
+            exact
+            component={() =>
+              !user.result ? <Auth /> : <Redirect to="posts" />
+            }
+          />
         </Switch>
       </Container>
     </BrowserRouter>
